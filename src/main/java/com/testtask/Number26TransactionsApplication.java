81ab153df336530432dab1a9baf2e156a7b51b4b
etsatsina <e.tsatsina@ya.ru>
2016-07-29 13:49:10 +0200

    generated spring boot app

---
+++ src/main/java/com/testtask/Number26TransactionsApplication.java
@@ -0,0 +1,12 @@
+package com.testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Number26TransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Number26TransactionsApplication.class, args);
+	}
+}
