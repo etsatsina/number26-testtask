7dca646298c8b03721fd51642b872ed306b8d724
etsatsina <e.tsatsina@ya.ru>
2016-07-30 17:24:25 +0200

    added domain class

---
+++ src/main/java/com/testtask/model/domain/Transaction.java
@@ -0,0 +1,27 @@
+package com.testtask.model.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by etsatsina on 29-Jul-16.
+ */
+@EqualsAndHashCode(exclude = "childrenIds")
+@Setter
+@Getter
+public class Transaction {
+
+    private Long id;
+
+    private Double amount;
+
+    private String type;
+
+    private Long parentId;
+
+    Set<Long> childTransactionsIds = new HashSet<>();
+}
