5c2db5b383d3583a6ee294e9ccba02fb92f4d30e
etsatsina <e.tsatsina@ya.ru>
2016-07-31 15:45:52 +0200

    added response statuses for controller

--- src/main/java/com/testtask/controllers/TransactionController.java
+++ src/main/java/com/testtask/controllers/TransactionController.java
@@ -5,8 +5,10 @@ import com.testtask.model.dto.TotalTransactionAmountDto;
 import com.testtask.model.dto.TransactionDto;
 import com.testtask.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -23,22 +25,27 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
-    @RequestMapping(value = "/transaction/{transactionId}", method = RequestMethod.PUT)
-    public void create(@PathVariable("transactionId") Long id, TransactionDto transactionDto) {
+    @RequestMapping(value = "/transaction/{transactionId}", method = RequestMethod.PUT, consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@PathVariable("transactionId") Long id,
+                       @Valid @RequestBody TransactionDto transactionDto) {
         transactionService.save(id, transactionDto);
     }
 
     @RequestMapping(value = "/transaction/{transactionId}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public TransactionDto getById(@PathVariable("transactionId") Long id) {
         return transactionService.get(id);
     }
 
     @RequestMapping(value = "/types/{type}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public List<Long> getByType(@PathVariable("type") String type) {
         return transactionService.findAllByType(type);
     }
 
     @RequestMapping(value = "/sum/{transactionId}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public TotalTransactionAmountDto getTotalTransactionAmount(@PathVariable("transactionId") Long id) {
         return transactionService.getTotalTransactionAmount(id);
     }
